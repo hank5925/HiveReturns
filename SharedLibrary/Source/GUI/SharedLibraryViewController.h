9a5e26c8de837fd7129f57398b4e175d4696b57d
hank5925 <hank5925@gmail.com>
2014-03-10 22:04:41 -0400

    Update GUI and passing device motion data

--- SharedLibrary/Source/GUI/SharedLibraryViewController.h
+++ SharedLibrary/Source/GUI/SharedLibraryViewController.h
@@ -7,20 +7,46 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <CoreMotion/CoreMotion.h>
 #include "SharedLibraryInterface.h"
+#include "SharedLibraryHeader.h"
 
 @interface SharedLibraryViewController : UIViewController
 {
     SharedLibraryInterface*     backEndInterface;
     
-    bool m_bAudioToggleStatus;
-    int  m_iRecordPlaybackStatus;
+    bool m_bToggleStartStatus;
+    bool m_bToggleVibratoStatus;
+    bool m_bTogglePitchShiftStatus;
+    bool m_bToggleDelayStatus;
+    bool m_bToggleLowpassStatus;
 }
 
+// Core Motion
+@property (nonatomic, strong) CMMotionManager* motionManager;
+- (void) motionDeviceUpdate:(CMDeviceMotion *)deviceMotion;
+
+// GUI
+@property (retain, nonatomic) IBOutlet UIButton *toggleStartButton;
+- (IBAction)toggleStartButtonClicked:(UIButton *)sender;
+
+@property (retain, nonatomic) IBOutlet UIButton *toggleVibratoButton;
+- (IBAction)toggleVibratoButtonClicked:(UIButton *)sender;
+
+@property (retain, nonatomic) IBOutlet UIButton *togglePitchShiftButton;
+- (IBAction)togglePitchShiftButtonClicked:(UIButton *)sender;
+
+@property (retain, nonatomic) IBOutlet UIButton *toggleDelayButton;
+- (IBAction)toggleDelayButtonClicked:(UIButton *)sender;
+
+@property (retain, nonatomic) IBOutlet UIButton *toggleLowpassButton;
+- (IBAction)toggleLowpassButtonClicked:(UIButton *)sender;
 
 
-@property (retain, nonatomic) IBOutlet UIButton *toggleAudioButton;
-- (IBAction)toggleAudioButtonClicked:(UIButton *)sender;
+
+
+
+
 
 
 
